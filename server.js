9a927c1894854917d3a35cce7f26bc50a0184b73
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-20 21:35:44 -0400

    created server Configuration

--- server.js
+++ server.js
@@ -2,3 +2,23 @@ import express from "express";
 import connectDB from "./db/connection";
 import Book from "./models/Book";
 import router from "./routes/bookRoutes";
+
+dotenv.config();
+
+const app = express();
+
+//middleware
+app.use(express.json());
+
+//DB Connect
+connectDB();
+
+//routes
+app.use('/api/books', booksRoutes);
+
+// server start
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
+});
